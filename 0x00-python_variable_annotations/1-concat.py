c88a3ecb04448442b5e27e5bdf9ccb8bca54038d
wac <wabaham9@gmail.com>
2023-04-27 17:00:50 +0000

    correction

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,10 +1,9 @@
-#!/bin/usr/env python3
-"""Write a type-annotated function concat that takes a string str1
-    and a string str2 as arguments and returns a concatenated string
-    """
+#!/usr/bin/env python3
+'''Task 1's module.
+'''
 
 
 def concat(str1: str, str2: str) -> str:
-    """ returns concatinated string of str1 and str2 """
-
-    return "{}{}".format(str1, str2)
+    '''Concatenates two strings.
+    '''
+    return str1 + str2
