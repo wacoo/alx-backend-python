82d0eb54b997f762b7437a515d1f1196b421ce4f
wac <wabaham9@gmail.com>
2023-04-27 17:14:10 +0000

    task 1 correction 5

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
 #!/bin/usr/env python3
-'''Write a type-annotated function concat that takes a string str1
+''' Write a type-annotated function concat that takes a string str1
     and a string str2 as arguments and returns a concatenated string
-    '''
+'''
 
 
 def concat(str1: str, str2: str) -> str:
