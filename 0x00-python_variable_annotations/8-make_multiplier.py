2578b53de222004eefbc7ee1e4b50fd1fab4119b
wac <wabaham9@gmail.com>
2023-04-27 02:26:38 -0700

    task 8

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,5 +3,8 @@
 as argument and returns a function that multiplies a float by multiplier. """
 
 
-def make_multiplier(multiplier: float) -> function:
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
     return lambda x: x * multiplier
