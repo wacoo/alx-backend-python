7c3efd6604b1a4685780c21c27d84d50fd437c9a
wac <wabaham9@gmail.com>
2023-04-27 17:19:10 +0000

    task 1 correction 5

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,5 @@
 #!/bin/usr/env python3
-''' Write a type-annotated function concat that takes a string str1
-    and a string str2 as arguments and returns a concatenated string
+''' Task one answer
 '''
 
 
