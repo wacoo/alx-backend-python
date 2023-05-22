80c067aaf54f6d609ae37feb88a06eb15ff74675
wac <wabaham9@gmail.com>
2023-05-22 17:54:46 +0800

    task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,17 +10,20 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
-class TestAccessNestedMap(unittest.TestCase):
-	''' TestAccessMap test class that inherits from TestCase '''
-	def setUp(self):
-		self.nested_map = {"a": {"b": {"c": 1}}}
-	@parameterized.expand([
-		({"a": 1}, ("a",), 1),
-		({"a": {"b": 2}}, ("a",), {"b": 2}),
-		({"a": {"b": 2}}, ("a", "b"), 2)
-	])
 
-	def test_access_nested_map(self, nested, path, expected):
-		''' tests if the function access_nested_map
-		returns what it is suppose to '''
-		self.assertEqual(access_nested_map(nested, path), expected)
+class TestAccessNestedMap(unittest.TestCase):
+    ''' TestAccessMap test class that inherits from TestCase '''
+
+    def setUp(self):
+        ''' setup test data '''
+        self.nested_map = {"a": {"b": {"c": 1}}}
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested, path, expected):
+        ''' tests if the function access_nested_map
+        returns what it is suppose to '''
+        self.assertEqual(access_nested_map(nested, path), expected)
