4434c106277e72b4011537bc14b65b2a0978eb08
wac <wabaham9@gmail.com>
2023-05-22 15:25:32 +0300

    task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,7 +9,8 @@ following inputs:
 import json
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
+
 from typing import Dict, Union, Tuple
 from unittest.mock import patch, MagicMock
 
@@ -35,7 +36,11 @@ class TestAccessNestedMap(unittest.TestCase):
             ({}, ("a",)),
             ({"a": 1}, {"a", "b"})
         ])
-    def test_access_nested_map_exception(self, nested_map, path):
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Union[Dict, Tuple]
+            ) -> None:
         ''' tests for KeyError exception '''
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
@@ -48,7 +53,7 @@ class TestGetJson(unittest.TestCase):
             ("http://example.com", {"payload": True}),
             ("http://holberton.io", {"payload": False})
         ])
-    def test_get_json(self, test_url, test_payload):
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
         ''' test the results of utils.get_json ,use Mock '''
         with patch('utils.requests.get') as mock_requests:
             res = MagicMock()
@@ -56,3 +61,28 @@ class TestGetJson(unittest.TestCase):
             res.json.return_value = test_payload
             mock_requests.return_value = res
             self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    ''' class that tests if a_method is called once inside a_property '''
+    def test_memoize(self):
+        ''' test that when calling a_property_] twice, the correct
+        result is returned but a_method is called only once using
+        assert_called_once '''
+        class TestClass:
+            def a_method():
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42
+                ) as mem_func:
+            t_cls = TestClass()
+            self.assertEqual(t_cls.a_property(), 42)
+            self.assertEqual(t_cls.a_property(), 42)
+            mem_func.assert_called_once()
